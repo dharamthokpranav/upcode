45e2b5895551c6e75de931f698f4a2bfa2c32565
hardikjhamb17 <hardikjhamb17@gmail.com>
2022-07-13 00:05:11 +0530

    feat: added getPatientConsultationData API

--- services/DoctorDashboardService.js
+++ services/DoctorDashboardService.js
@@ -25,6 +25,28 @@ class DoctorDashboardService{
             dbaccess.closeConnection(connection);
         }
     }
+
+    async getPatientConsultationData(userInfo, result) {
+        const connection = dbaccess.openConnection();
+        try {
+            //select * from table_name where where email=? and password=?
+            connection.query("SELECT * FROM employee LEFT JOIN employeedata ON employee.empid = employeedata.empid LEFT JOIN booksdata ON employee.empid = booksdata.empid where employee.empid=?", [userInfo.empid], function (err, res) {
+                if (err) {
+                    result(err, null);
+                }
+                else {
+                    result(null, res);
+                }
+            
+            });
+        }
+        catch (error) {
+            console.log("Method:LoginUser,File:appservice.js--> " + error);
+        }
+        finally {
+            dbaccess.closeConnection(connection);
+        }
+    }
    
 
 
