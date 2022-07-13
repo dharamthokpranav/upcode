705b24beef89c0cf9ae92d946d9ef04d60127540
hardikjhamb17 <hardikjhamb17@gmail.com>
2022-07-13 16:23:21 +0530

    feat: added update patient consultation

--- services/DoctorDashboardService.js
+++ services/DoctorDashboardService.js
@@ -1,54 +1,77 @@
-const dbaccess= require("../database/dbaccess");
-class DoctorDashboardService{
+const dbaccess = require("../database/dbaccess");
+class DoctorDashboardService {
+  constructor() {}
 
-    constructor()
-    {}
-
-    async loginDoctor(userInfo, result) {
-        const connection = dbaccess.openConnection();
-        try {
-            //select * from table_name where where email=? and password=?
-            connection.query("select * from table_name where where email=? and password=?", [userInfo.email, userInfo.password], function (err, res) {
-                if (err) {
-                    result(err, null);
-                }
-                else {
-                    result(null, res);
-                }
-            
-            });
+  async loginDoctor(userInfo, result) {
+    const connection = dbaccess.openConnection();
+    try {
+      //select * from table_name where where email=? and password=?
+      connection.query(
+        "select * from table_name where where email=? and password=?",
+        [userInfo.email, userInfo.password],
+        function (err, res) {
+          if (err) {
+            result(err, null);
+          } else {
+            result(null, res);
+          }
         }
-        catch (error) {
-            console.log("Method:LoginUser,File:appservice.js--> " + error);
-        }
-        finally {
-            dbaccess.closeConnection(connection);
-        }
+      );
+    } catch (error) {
+      console.log("Method:LoginUser,File:appservice.js--> " + error);
+    } finally {
+      dbaccess.closeConnection(connection);
     }
+  }
 
-    async getPatientConsultationData(userInfo, result) {
-        const connection = dbaccess.openConnection();
-        try {
-            //select * from table_name where where email=? and password=?
-            connection.query("SELECT * FROM pres_master_prescription LEFT JOIN pres_data ON pres_master_prescription.diagnosis_id = pres_data.end_result_id WHERE pres_master_prescription.diagnosis_id = ?", [userInfo.diagnosis_id], function (err, res) {
-                if (err) {
-                    result(err, null);
-                }
-                else {
-                    result(null, res);
-                }
-            
-            });
+  async getPatientConsultationData(userInfo, result) {
+    const connection = dbaccess.openConnection();
+    try {
+      //select * from table_name where where email=? and password=?
+      connection.query(
+        "SELECT * FROM pres_master_prescription LEFT JOIN pres_data ON pres_master_prescription.diagnosis_id = pres_data.end_result_id WHERE pres_master_prescription.diagnosis_id = ?",
+        [userInfo.diagnosis_id],
+        function (err, res) {
+          if (err) {
+            result(err, null);
+          } else {
+            result(null, res);
+          }
         }
-        catch (error) {
-            console.log("Method:LoginUser,File:appservice.js--> " + error);
-        }
-        finally {
-            dbaccess.closeConnection(connection);
-        }
+      );
+    } catch (error) {
+      console.log("Method:LoginUser,File:appservice.js--> " + error);
+    } finally {
+      dbaccess.closeConnection(connection);
     }
-   
+  }
 
-
+  async updatePatientConsultationData(updateData, result) {
+    const connection = dbaccess.openConnection();
+    try {
+      //select * from table_name where where email=? and password=?
+      connection.query(
+        " UPDATE pres_master_prescription SET pres_master_prescription.diagnosis = CASE WHEN ?='' THEN pres_master_prescription.diagnosis ELSE ? END, pres_master_prescription.medicine_prescribed = CASE WHEN ?='' THEN pres_master_prescription.medicine_prescribed ELSE ? END WHERE pres_master_prescription.diagnosis_id = ?",
+        [
+          updateData.diagnosis,
+          updateData.diagnosis,
+          updateData.medicine_prescribed,
+          updateData.medicine_prescribed,
+          updateData.diagnosis_id,
+        ],
+        function (err, res) {
+          if (err) {
+            result(err, null);
+          } else {
+            result(null, res);
+          }
+        }
+      );
+    } catch (error) {
+      console.log("Method:LoginUser,File:appservice.js--> " + error);
+    } finally {
+      dbaccess.closeConnection(connection);
+    }
+  }
 }
-module.exports=DoctorDashboardService;
+module.exports = DoctorDashboardService;
